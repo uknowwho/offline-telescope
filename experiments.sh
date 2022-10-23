61726e96f2e98cc982344b65a6482fa9b2e8b6b2
Florian van der Steen <florianvandersteen@gmail.com>
2022-10-23 20:17:51 +0200

    Now also installs required python packages

--- experiments.sh
+++ experiments.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
+pip3 install -r requirements.txt
 unzip good_instances.zip
 mkdir solutions
 python3 experiments.py
-
-# for directory in Instances/Instances_*/
-# do
-#   for FILE in "$directory"*txt
-#   do
-# 	echo "solving $FILE"
-#     prefix=$directory
-#     suffix=".txt"
-# 	string=$FILE
-# 	no_instance=${string#"$prefix"}
-# 	no_txt=${no_instance%"$suffix"}
-#     python3 MLP.py "$FILE" $"solutions/${no_txt}-solution.txt" > notes
-# 	rm notes
-#   done
-# done
-
